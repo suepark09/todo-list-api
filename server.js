92ebfc27b62c3fce62f656e712ba797cdc033d7a
Sue Park <suepandrice06@gmail.com>
2019-09-19 08:00:54 -0500

    added post request, currently working on put

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+let Joi = require('joi');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,15 +13,69 @@ var todoList = [
 ];
 
 // GET /api/todos
+app.get("/api/todos", function (request, response) {
+    console.log(request.params);
+    response.send(todoList)
+})
 
 // GET /api/todos/:id
+app.get("/api/todos/:id", function (request, response, nextFn) {
+    const todoItem = todoList.find(function(item) {
+        return item.id === parseInt(request.params.id)
+    })
+
+    if (!todoItem) {
+        response.status(404)
+        nextFn()
+    } else {
+        response.send(todoItem)
+    }
+})
 
 // POST /api/todos
 
+app.post("/api/todos", function (request, response, nextFn) {
+    const schema = {
+        todo: Joi.string().min(3).required()
+    };
+
+    const result = Joi. validate(request.body, schema);
+
+    if (result.error) {
+        response.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const todos = {
+        id: todoList.length + 1,
+        todo: request.body.todo
+    };
+
+    todoList.push(todos);
+    response.send(todos);
+})
+
 // PUT /api/todos/:id
+app.put('/api/todos/:id', function (request, response) {
+    const todos = todoList.find(function (item) {
+        return item.id === parseInt(request.params.id);
+    })
+
+    if (!todos) {
+        response.status(404).send('the to do was not found');
+    } 
+
+    todoList.todos = request.body.todos;
+    response.send(todos);
+
+})
+
 
 // DELETE /api/todos/:id
+app.delete('/api/todos/:id')
 
-app.listen(3000, function(){
-    console.log('Todo List API is now listening on port 3000...');
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+    console.log(`listening in port ${port}...`)
 })
